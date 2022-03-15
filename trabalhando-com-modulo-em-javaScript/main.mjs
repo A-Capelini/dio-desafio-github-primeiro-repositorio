dbb2d5617ce1a5a877c2919bd410242dd1fe6505
A-Capelini <anderson_capelini@yahoo.com.br>
2022-03-15 19:17:05 -0300

    Inserido arquivos index.html, funcoes.mjs e main.mjs

---
+++ trabalhando-com-modulo-em-javaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Anderson', 42));
+console.log(mostraCidade('Anderson', 'Osasco'));
+console.log(mostraHobby('Anderson', 'Marcenaria'));
